c1a9123d35f046b1242527ff7bd683256baefb01
GodoyMS <youremail@domain.com>
2023-06-01 15:35:58 -0500

    sd

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 app.use(
   cors({
-    origin:'*',
+    origin:'https://buscador-de-clientes.vercel.app',
     credentials: true,
     //optionSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
